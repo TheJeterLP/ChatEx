af1d496343d5da4728a4290e4326c901527a872c
TheJeterLP <joey.peter1998@gmail.com>
2020-05-27 22:11:20 +0200

    [ADD] Add licenseheader [ADD] Add more files to .gitignore [ADD] Add @worldOneo to plugin.yml [MOD] Change PlaceholderAPI access, cleanup

--- src/main/java/de/jeter/chatex/PlayerListener.java
+++ src/main/java/de/jeter/chatex/PlayerListener.java
@@ -1,7 +1,27 @@
+/*
+ * This file is part of ChatEx
+ * Copyright (C) 2020 ChatEx Team
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package de.jeter.chatex;
 
-import de.jeter.chatex.utils.*;
-import me.clip.placeholderapi.PlaceholderAPI;
+import de.jeter.chatex.utils.Config;
+import de.jeter.chatex.utils.Locales;
+import de.jeter.chatex.utils.UpdateChecker;
+import de.jeter.chatex.utils.Utils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -20,11 +40,6 @@ public class PlayerListener implements Listener {
 
         if (Config.CHANGE_TABLIST_NAME.getBoolean()) {
             String name = Config.TABLIST_FORMAT.getString();
-
-            if (HookManager.checkPlaceholderAPI()) {
-                name = PlaceholderAPI.setPlaceholders(e.getPlayer(), name);
-            }
-
             name = Utils.replacePlayerPlaceholders(e.getPlayer(), name);
             e.getPlayer().setPlayerListName(name);
         }
--- src/main/java/de/jeter/chatex/utils/adManager/SmartAdManager.java
+++ src/main/java/de/jeter/chatex/utils/adManager/SmartAdManager.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of ChatEx
+ * Copyright (C) 2020 ChatEx Team
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package de.jeter.chatex.utils.adManager;
 
 import de.jeter.chatex.ChatEx;
@@ -11,7 +29,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SmartAdManager implements AdManager {
-    private static Map<UUID, Double> uuidErrorMap = new HashMap<>();
+    private static final Map<UUID, Double> uuidErrorMap = new HashMap<>();
     private static final Pattern ipPattern = Pattern.compile("(\\d{1,3}([.:\\-, ])?){4}");
     private static final Pattern webPattern = Pattern.compile("((([a-zA-Z0-9_-]{2,256}\\.)*)?[a-zA-Z0-9_-]{2,256}\\.[a-zA-Z0-9_-]{2,256})(\\/[-a-zA-Z0-9@:%_\\\\+~#?&\\/=]*)?");
 
@@ -61,6 +79,7 @@ public class SmartAdManager implements AdManager {
         return error;
     }
 
+    @Override
     public boolean checkForAds(String msg, Player p) {
         if (p.hasPermission("chatex.bypassads")) {
             return false;
