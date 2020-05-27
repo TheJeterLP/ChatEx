e269dde79085dd67c6beb5bd49add8aafaf67214
worldOneo <62426504+worldOneo@users.noreply.github.com>
2020-05-27 23:23:03 +0200

    [MOD] ChatExAPI.java added every PluginManager function.

--- src/main/java/de/jeter/chatex/api/ChatExAPI.java
+++ src/main/java/de/jeter/chatex/api/ChatExAPI.java
@@ -1,15 +1,36 @@
 package de.jeter.chatex.api;
 
-import de.jeter.chatex.api.events.EventManager;
 import de.jeter.chatex.api.events.DefaultEventManager;
+import de.jeter.chatex.api.events.EventManager;
+import de.jeter.chatex.plugins.PluginManager;
+import org.bukkit.entity.Player;
 
 public class ChatExAPI {
-    public ChatExAPI() {
+    public String getPermissionHandlerName() {
+        return PluginManager.getInstance().getName();
     }
 
     public EventManager getEventManager() {
         return DefaultEventManager.getInstance();
     }
 
+    public String getPrefix(Player p) {
+        return PluginManager.getInstance().getPrefix(p);
+    }
 
+    public String getSuffix(Player p) {
+        return PluginManager.getInstance().getSuffix(p);
+    }
+
+    public String[] getGroupNames(Player p) {
+        return PluginManager.getInstance().getGroupNames(p);
+    }
+
+    public String getMessageFormat(Player p) {
+        return PluginManager.getInstance().getMessageFormat(p);
+    }
+
+    public String getGlobalMessageFormat(Player p) {
+        return PluginManager.getInstance().getGlobalMessageFormat(p);
+    }
 }
