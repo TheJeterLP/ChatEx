c4e3f9964245b55822db529cc168b9f75dd97145
TheJeterLP <joey.peter1998@gmail.com>
2020-05-28 20:12:59 +0200

    [MOD] Cleanup [ADD] Add license header to API

--- src/main/java/de/jeter/chatex/api/events/MessageBlockedBySpamManagerEvent.java
+++ src/main/java/de/jeter/chatex/api/events/MessageBlockedBySpamManagerEvent.java
@@ -1,13 +1,32 @@
+/*
+ * This file is part of ChatEx
+ * Copyright (C) 2020 ChatEx Team
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 package de.jeter.chatex.api.events;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 
 public class MessageBlockedBySpamManagerEvent extends ChatExEvent {
+    
     private static final HandlerList handlers = new HandlerList();
-    private Player player;
+    private final Player player;
+    private final long remainingTime;
     private String message;
-    private long remainingTime;
     private String pluginMessage;
     private boolean canceled = false;
 
