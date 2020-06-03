a36c5081615ef3e9c8873220f493f1385194fd57
worldOneo <62426504+worldOneo@users.noreply.github.com>
2020-06-03 14:57:35 +0200

    [ADD] Tab completion for /chatex command in CommandHandler.java

--- src/main/java/de/jeter/chatex/CommandHandler.java
+++ src/main/java/de/jeter/chatex/CommandHandler.java
@@ -1,17 +1,17 @@
 /*
  * This file is part of ChatEx
  * Copyright (C) 2020 ChatEx Team
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
@@ -25,7 +25,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
-public class CommandHandler implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandHandler implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -44,7 +47,7 @@ public class CommandHandler implements CommandExecutor {
 
                     if (Config.CHANGE_TABLIST_NAME.getBoolean()) {
                         for (Player p : Bukkit.getOnlinePlayers()) {
-                            String name = Config.TABLIST_FORMAT.getString();                          
+                            String name = Config.TABLIST_FORMAT.getString();
                             name = Utils.replacePlayerPlaceholders(p, name);
                             p.setPlayerListName(name);
                         }
@@ -83,5 +86,17 @@ public class CommandHandler implements CommandExecutor {
             }
         }
 
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        List<String> possibleTabs = new ArrayList<>();
+        if (commandSender.hasPermission("chatex.clear")) {
+            possibleTabs.add("clear");
+        }
+        if (commandSender.hasPermission("chatex.reload")) {
+            possibleTabs.add("reload");
+        }
+        return possibleTabs;
     }
 }
