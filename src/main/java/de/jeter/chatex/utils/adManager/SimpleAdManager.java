cf32e2c9f31ab1cc554a81cfa41d219f617f0f12
worldOneo <62426504+worldOneo@users.noreply.github.com>
2020-07-08 22:23:53 +0200

    [ADD] Utils#notifyOps to send a message to the operators. [MOD] SmartAdManager.java, SimpleAdManager.java to use Utils#notifyOps [FIX] SmartAdManager.java, SimpleAdManager.java Quoting player message and name to avoid `java.lang.IllegalArgumentException` (Fix for https://github.com/TheJeterLP/ChatEx/issues/27)

--- src/main/java/de/jeter/chatex/utils/adManager/SimpleAdManager.java
+++ src/main/java/de/jeter/chatex/utils/adManager/SimpleAdManager.java
@@ -1,24 +1,23 @@
 /*
  * This file is part of ChatEx
  * Copyright (C) 2020 ChatEx Team
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package de.jeter.chatex.utils.adManager;
 
-import de.jeter.chatex.ChatEx;
 import de.jeter.chatex.utils.ChatLogger;
 import de.jeter.chatex.utils.Config;
 import de.jeter.chatex.utils.Locales;
@@ -93,13 +92,10 @@ public class SimpleAdManager implements AdManager {
         }
         boolean found = checkForIPPattern(msg) || checkForWebPattern(msg);
         if (found) {
-            for (Player op : ChatEx.getInstance().getServer().getOnlinePlayers()) {
-                if (!op.hasPermission("chatex.notifyad")) {
-                    continue;
-                }
-                String message = Locales.MESSAGES_AD_NOTIFY.getString(p).replaceAll("%player", p.getName()).replaceAll("%message", msg);
-                op.sendMessage(message);
-            }
+            String message = Locales.MESSAGES_AD_NOTIFY.getString(p)
+                    .replaceAll("%player", Matcher.quoteReplacement(p.getName()))
+                    .replaceAll("%message", Matcher.quoteReplacement(msg));
+            Utils.notifyOps(message);
             ChatLogger.writeToAdFile(p, msg);
         }
         return found;
--- src/main/java/de/jeter/chatex/utils/adManager/SmartAdManager.java
+++ src/main/java/de/jeter/chatex/utils/adManager/SmartAdManager.java
@@ -1,24 +1,23 @@
 /*
  * This file is part of ChatEx
  * Copyright (C) 2020 ChatEx Team
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package de.jeter.chatex.utils.adManager;
 
-import de.jeter.chatex.ChatEx;
 import de.jeter.chatex.utils.*;
 import org.bukkit.entity.Player;
 
@@ -69,7 +68,7 @@ public class SmartAdManager implements AdManager {
                         error += text.length();
                         if (DomainDictionary.containsTopLevelEnding(text)) {
                             error *= Config.ADS_SMART_MULTIPLIER.getDouble();
-                        }else{
+                        } else {
                             error *= Config.ADS_SMART_UN_MULTIPLIER.getDouble();
                         }
                     }
@@ -97,14 +96,10 @@ public class SmartAdManager implements AdManager {
         boolean canceled = uuidErrorMap.get(p.getUniqueId()) > Config.ADS_THRESHOLD.getDouble() || checkForIPPattern(msg);
         if (canceled) {
             uuidErrorMap.put(p.getUniqueId(), Config.ADS_THRESHOLD.getDouble());
-            for (Player op : ChatEx.getInstance().getServer().getOnlinePlayers()) {
-                if (!op.hasPermission("chatex.notifyad")) {
-                    continue;
-                }
-
-                String message = Locales.MESSAGES_AD_NOTIFY.getString(p).replaceAll("%player", p.getName()).replaceAll("%message", msg);
-                op.sendMessage(message);
-            }
+            String message = Locales.MESSAGES_AD_NOTIFY.getString(p)
+                    .replaceAll("%player", Matcher.quoteReplacement(p.getName()))
+                    .replaceAll("%message", Matcher.quoteReplacement(msg));
+            Utils.notifyOps(message);
             ChatLogger.writeToAdFile(p, msg);
         } else {
             uuidErrorMap.put(p.getUniqueId(), uuidErrorMap.get(p.getUniqueId()) - Config.ADS_REDUCE_THRESHOLD.getDouble());
