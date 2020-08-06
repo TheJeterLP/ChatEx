fa257ced787416ff58712ef320662a0598a52fa7
worldOneo <62426504+worldOneo@users.noreply.github.com>
2020-08-06 15:53:27 +0200

    Fixed wrong labled/wrong fired PlayerUsesRangeModeEvent [ADD] PlayerUsesGlobalChatEvent [MOD] ChatListener fixing the events

---
+++ src/main/java/de/jeter/chatex/api/events/PlayerUsesGlobalChatEvent.java
@@ -0,0 +1,61 @@
+package de.jeter.chatex.api.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PlayerUsesGlobalChatEvent extends Event implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+    private String message;
+    private final Player player;
+    private boolean canceled = false;
+
+    public PlayerUsesGlobalChatEvent(Player player, String message){
+        super(true);
+        this.player = player;
+        this.message = message;
+    }
+
+    /**
+     * @return the player which fired this event
+     */
+    public Player getPlayer() {
+        return player;
+    }
+
+    /**
+     * @return the message which the player would have written.
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     *
+     * @param message set the message which the player writes.
+     */
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return canceled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        canceled = b;
+    }
+}
