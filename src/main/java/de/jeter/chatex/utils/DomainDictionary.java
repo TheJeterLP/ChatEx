2b257ba24f5acd9d7c59c88f52ffffc7b22b21bb
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2022-10-26 14:32:25 +0200

    Implemented asynchronous code.
    Used java threads to make code run faster.

--- src/main/java/de/jeter/chatex/utils/DomainDictionary.java
+++ src/main/java/de/jeter/chatex/utils/DomainDictionary.java
@@ -20,21 +20,26 @@ package de.jeter.chatex.utils;
 
 import java.util.HashSet;
 
+import java.lang.Thread;
+
 public class DomainDictionary {
 
     private static final HashSet<String> endingSet = new HashSet<>(Config.ADS_SMART_DOMAIN_ENDINGS.getStringList());
 
     public static boolean containsTopLevelEnding(String checkString) {
-        String[] parts = checkString.split("\\.");
-        String ending = parts[parts.length - 1];
-        StringBuilder stringBuilder = new StringBuilder();
-        for (char Character : ending.toCharArray()) {
-            stringBuilder.append(Character);
-            if (endingSet.contains(stringBuilder.toString())) {
-                return true;
+        Thread topLevelEnding = new Thread(() -> {
+            String[] parts = checkString.split("\\.");
+            String ending = parts[parts.length - 1];
+            StringBuilder stringBuilder = new StringBuilder();
+            for (char Character : ending.toCharArray()) {
+                stringBuilder.append(Character);
+                if (endingSet.contains(stringBuilder.toString())) {
+                    return true;
+                }
             }
-        }
-        return false;
+            return false;
+        });
+        topLevelEnding.start();
     }
 
 }
