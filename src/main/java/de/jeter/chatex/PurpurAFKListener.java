c421ecf1ba76578f2c1b4cfcbaff74a13358b68d
FlameyosFlow <49708496+coderFlameyosFlow@users.noreply.github.com>
2022-10-26 12:19:36 +0200

    Implemented asynchronous code.
    Used java threads to make code run faster.

--- src/main/java/de/jeter/chatex/PurpurAFKListener.java
+++ src/main/java/de/jeter/chatex/PurpurAFKListener.java
@@ -7,16 +7,21 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.purpurmc.purpur.event.PlayerAFKEvent;
 
+import java.lang.Thread;
+
 public class PurpurAFKListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onAFKChangeEvent(PlayerAFKEvent event) {
-        Player player = event.getPlayer();
-        if (event.isGoingAfk()) {
-            String format = Config.TABLIST_FORMAT.getString().replace("%afk", Config.AFK_FORMAT.getString());
-            player.setPlayerListName(Utils.replacePlayerPlaceholders(player, format));
-            return;
-        }
-        player.setPlayerListName(Utils.replacePlayerPlaceholders(player, Config.TABLIST_FORMAT.getString()));
+        Thread purpurAFKEvent = new Thread(() -> {
+            Player player = event.getPlayer();
+            if (event.isGoingAfk()) {
+                String format = Config.TABLIST_FORMAT.getString().replace("%afk", Config.AFK_FORMAT.getString());
+                player.setPlayerListName(Utils.replacePlayerPlaceholders(player, format));
+                return;
+            }
+            player.setPlayerListName(Utils.replacePlayerPlaceholders(player, Config.TABLIST_FORMAT.getString()));
+        });
+        purpurAFKEvent.start();
     }
 }
